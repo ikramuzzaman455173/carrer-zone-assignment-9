5d45d4bfaa803259090fdbeb05b9b9d56c45130a
jakaria455173 <jakaria455173@gmail.com>
2023-04-12 19:56:22 +0600

    Banner Image Add Hero Sections

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const HeroSection = ({children}) => {
   return (
     <div className='hero-section bg-slate-50 mb-5 pb-5'>
+      <div className="img-1"><img src="https://i.ibb.co/3pYGLrT/banner.png" alt="banner-img" /></div>
+      <div className="img-2"><img src="https://i.ibb.co/d07MJWs/banner-1.png" alt="banner-img1" /></div>
       <h3 className='text-center font-bold text-3xl my-5'>{children}</h3>
     </div>
   )
